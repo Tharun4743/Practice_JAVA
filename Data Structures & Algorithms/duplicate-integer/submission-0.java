7e5fd1bb45aebaf50e9a57af09bb0ff9e755b40a
Tharunkumar K <tharunkumark42007@gmail.com>
2026-06-15 09:53:49 +0530

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+        
+        HashSet<Integer> a =new HashSet<>();
+        for(int num:nums){
+            if(a.contains(num)){
+
+                return true;
+            }
+            a.add(num);
+        }
+        return false;
+    }
+}
